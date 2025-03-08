a0bcc18c85ec51b006a3d2b764ef9c79072228fa
tuhinhossain123 <csetuhin55@gmail.com>
2025-03-08 11:23:57 +0600

    basic get and post request practice

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
+const express = require("express");
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// parsers
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello Dev");
+});
+app.get("/", (req, res) => {
+    console.log(req.body);
+    res.send("get data");
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
+import { Request, Response } from "express";
+const express = require("express");
+const app = express();
 
+// parsers
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Dev");
+});
+
+app.get("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send("get data");
+});
 export default app;
